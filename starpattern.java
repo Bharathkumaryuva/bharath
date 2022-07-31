4562613499054423d816b37ce6c50af421cbdf73
Bharathkumaryuva <109011816+Bharathkumaryuva@users.noreply.github.com>
2022-07-31 11:19:53 +0530

    Add files via upload

---
+++ starpattern.java
@@ -0,0 +1,18 @@
+class starpattern {
+
+    public static void main(String args[]) {
+
+        int a = 1;
+        int i;
+        for (i = 0; i < 3; i++) {
+            System.out.print(" ");
+        }
+
+        for (int j = 0; j <= 3; j++) {
+            System.out.print(a + " ");
+            a++;
+        }
+        System.out.println(" ");
+
+    }
+}
