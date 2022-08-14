fd193dedd902dcca9fc28895946d43bf3eb7c9e7
Bharathkumaryuva <109011816+Bharathkumaryuva@users.noreply.github.com>
2022-08-14 11:44:40 +0530

    Add files via upload

---
+++ school.java
@@ -0,0 +1,20 @@
+class school{
+
+    int standard;
+    String gender,section;
+
+    viod student{
+        System.out.println("Hi I am "+gender+ )
+    }
+    public static void main(String[] args) {
+        
+        school s = new school();
+        System.out.println(s.standard);
+        System.out.println(s.gender);
+        System.out.println(s.section);
+        s.student();
+
+
+
+    }
+}
