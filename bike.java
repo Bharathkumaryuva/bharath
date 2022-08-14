46e42902699f3de89ac903d71b94c0cf67b09a0d
Bharathkumaryuva <109011816+Bharathkumaryuva@users.noreply.github.com>
2022-08-14 11:45:52 +0530

    Add files via upload

---
+++ bike.java
@@ -0,0 +1,15 @@
+public class bike{
+    String type;
+    int price,number;
+
+    public bike(){
+        type ="honda";
+        price = 65000;
+        number = 2343;
+        System.out.println(type + "  " + price + " " + number);
+    }
+    public static void main(String[] args) {
+        
+        carBike c5 = new carBike();
+    }
+}
